0d23d5807f8dba83aba71a6d1591cfb31fdfaf23
Trisha <t.hegde777@gmail.com>
2025-04-14 22:15:38 +0530

    Submit button in employeement record

--- frontend/src/pages/Faculty/subpages/EmploymentRecord.js
+++ frontend/src/pages/Faculty/subpages/EmploymentRecord.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './EmploymentRecord.css'
 
-
 const EmploymentRecord = () => {
   return (
     <div className="container">
-    <div className="header">
-      <h1 className='Recordbar'><strong>Employment Record</strong></h1>
-    </div>
+      <div className="header">
+        <h1 className='Recordbar'><strong>Employment Record</strong></h1>
+      </div>
+
+      <div className="info-box">
+        {/* You can add form fields here if needed */}
+        <button className="submit-btn" type="submit">Submit</button>
+      </div>
     </div>
   )
 }
